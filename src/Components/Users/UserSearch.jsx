b65cbdfb2254f0885abbb312f2f43e44f5cadc2e
“[Mohammad <you@example.com>
2024-02-14 13:50:58 +0530

    added a daisy-UI alert component using useState which display the alert only if the user tries to submit empty text

--- src/Components/Users/UserSearch.jsx
+++ src/Components/Users/UserSearch.jsx
@@ -4,17 +4,28 @@ import GithubContext from "../../Context/GithubContext";
 function UserSearch() {
   const { searchUsers, users, clearUser } = useContext(GithubContext);
   const [text, setText] = useState("");
-
+  const [alert, setAlert] = useState({ message: "", display: false });
   const handleChange = (e) => {
     setText(e.target.value);
+    setAlert({
+      message: "",
+      display: false,
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (text === "") {
-      alert("Please Enter Something");
+    if (text.trim() === "") {
+      setAlert({
+        message: "Please Enter Some Text!",
+        display: true,
+      });
     } else {
       searchUsers(text);
+      setAlert({
+        message: "",
+        display: false,
+      });
 
       setText("");
     }
@@ -26,6 +37,24 @@ function UserSearch() {
  md:grid-cols-2 mb-8 gap-8"
     >
       <div>
+        {alert.display && (
+          <div role="alert" className="alert alert-warning w-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              />
+            </svg>
+            <span className="text-2xl ">{alert.message}</span>
+          </div>
+        )}
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
